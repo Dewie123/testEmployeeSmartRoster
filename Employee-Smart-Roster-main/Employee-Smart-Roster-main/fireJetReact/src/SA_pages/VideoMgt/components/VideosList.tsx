8fff920c8a63b4b8d262b1b5b2cfec4c437eb463
Zhi Wei <wmlim014@gmail.com>
2025-05-10 03:45:40 +0800

    SA - Demo Video Management
    1. UI
    2. Condition (block user from retriggering upload button when uploading in progress)

--- Employee-Smart-Roster-main/Employee-Smart-Roster-main/fireJetReact/src/SA_pages/VideoMgt/components/VideosList.tsx
+++ Employee-Smart-Roster-main/Employee-Smart-Roster-main/fireJetReact/src/SA_pages/VideoMgt/components/VideosList.tsx
@@ -1,39 +1,54 @@
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import PrimaryButton from '../../../components/PrimaryButton/PrimaryButton';
+import SA_LandingMgtController from '../../../controller/SA_LandingMgtController'
 
 import { BiSolidShow } from '../../../../public/Icons.js'
 import '../VideoMgt.css'
 import '../../../../public/styles/common.css'
+import { useState } from 'react';
+import { any } from 'prop-types';
+
 
 interface AllVideoProps {
     videos: any;
-    updatePreviewVideo: (videoLink: string) => void
-    updateLandingVideo: (videoId: number, videoLink: string) => void
+    updatePreviewVideo: (url: string) => void
+    updateLandingVideo: (id: number, url: string) => void
 }
+
+const { filterIsShownVideo } = SA_LandingMgtController
 
 const AllVideos = ({videos, updatePreviewVideo, updateLandingVideo}: AllVideoProps) => {
     // console.log(videos)
     const location = useLocation()
     const isVideoMgt = location.pathname.includes('video-management');
+    const [ selectedVideo, setSelectedVideo ] = useState<number>()
+    useEffect(() => {
+        const video = filterIsShownVideo(videos, 1)
+        // console.log(video)
+        setSelectedVideo(video[0].id)
+    }, [videos])
 
     return(
         <div className={`${isVideoMgt ? 'set-visible' : 'App-mobile-responsive-table'}`}>
             {videos.map((video:any) => (
-            <div key={video.videoID}>
-                <div className="App-mobile-responsive-table-card">
+            <div key={video.id}>
+                <div 
+                    className={`App-mobile-responsive-table-card sa-view-video-display-card
+                        ${video.id === selectedVideo ? 'onhover' : ''}`}
+                    onClick={() => {
+                        updatePreviewVideo(video.url)
+                        setSelectedVideo(video.id)
+                    }}
+                >
                     <div className="App-mobile-responsive-table-card-data-detail uploaded-demo-video-list-item">
                         <h4>{video.title}</h4>
                         <div className="btns-grp">
-                            <BiSolidShow 
-                                className='view-video-icons'
-                                onClick={() => updatePreviewVideo(video.video_link)}
-                            />
                             {video.isShown === 0 ? (
                                 <PrimaryButton 
                                     text='Display on Landing'
-                                    onClick={() => updateLandingVideo(video.videoID, video.video_link)}
+                                    onClick={() => updateLandingVideo(video.id, video.url)}
                                 />
-                                    
                             ):(
                                 <p>Displayed in Landing</p>
                             )}
